388ab02e337465b1b5cea25de430426d2265ed45
Dhiraj Wishal <wishal.d@scopicsoftware.com>
2023-12-27 20:42:20 +0530

    Added better certificate support.

--- Source/Core/Certificate.cpp
+++ Source/Core/Certificate.cpp
@@ -1,5 +1,6 @@
 #include "Certificate.hpp"
 #include "Base64.hpp"
+#include "Logging.hpp"
 
 #include <chrono>
 #include <sstream>
@@ -8,6 +9,8 @@ namespace HiveCom
 {
     Certificate::Certificate(uint32_t version, const std::string &serial, const ByteView &publicKey,
                              const std::string &issuerName, const ByteView &privateKey, Dilithium3 &tool)
+        : m_publicKey(ToFixedBytes<Kyber768Key::PublicKeySize>(publicKey)), m_serial(serial), m_issuerName(issuerName),
+          m_version(version)
     {
         // Setup the raw certificate.
         std::stringstream rawCertificate;
@@ -23,5 +26,59 @@ namespace HiveCom
 
         // Finalize the certificate.
         m_certificate = rawCertificate.str();
+    }
+
+    Certificate::Certificate(std::string_view certificate, const ByteView &publicKey, Dilithium3 &tool)
+        : m_certificate(certificate.data())
+    {
+        // Split the certificate to splits.
+        const auto splits = splitCertificate(certificate);
+
+        // Validate the splits.
+        if (splits.size() < 6)
+        {
+            HC_LOG_FATAL("Invalid certificate!");
+            return;
+        }
+
+        // Decode the data.
+        m_version = std::stoi(splits[0]);
+        m_serial = splits[1];
+        m_issuerName = splits[2];
+        m_timestamp = splits[3];
+        m_publicKey = ToFixedBytes<Kyber768Key::PublicKeySize>(Base64(ToBytes(splits[4])).decode());
+        const auto signature = ToFixedBytes<Dilithium3::SignatureSize>(Base64(ToBytes(splits[5])).decode());
+
+        // Create the raw certificate.
+        std::string rawCertificate;
+        for (std::size_t i = 0; i < 5; i++)
+            rawCertificate += splits[i] + "\n";
+
+        // Verify the signature.
+        m_isValid = tool.verify(publicKey, signature, ToBytes(rawCertificate)) && isPeriodValid(m_timestamp);
+    }
+
+    std::vector<std::string> Certificate::splitCertificate(std::string_view certificate) const
+    {
+        std::string line;
+        std::vector<std::string> splits;
+        std::istringstream stream{certificate.data()};
+
+        while (std::getline(stream, line))
+            splits.emplace_back(std::move(line));
+
+        return splits;
+    }
+
+    bool Certificate::isPeriodValid(std::string_view timestamp) const
+    {
+        std::istringstream stream{timestamp.data()};
+
+        std::string line;
+        std::getline(stream, line);
+
+        std::chrono::sys_seconds seconds;
+        stream >> std::chrono::parse("%Y-%m-%dT%H:%M:%S%z", seconds);
+        return (std::chrono::system_clock::now() - std::chrono::months(ValidityPeriod)) < seconds;
     }
 } // namespace HiveCom
--- Source/Core/Certificate.hpp
+++ Source/Core/Certificate.hpp
@@ -2,9 +2,10 @@
 
 #include "Dilithium3.hpp"
 #include "Kyber768Key.hpp"
-#include "SHA256.hpp"
 
 #include <cstdint>
+#include <string_view>
+#include <vector>
 
 namespace HiveCom
 {
@@ -23,7 +24,9 @@ namespace HiveCom
     class Certificate final
     {
       public:
-        using DigestType = typename SHA256::DigestType;
+        static constexpr auto ValidityPeriod = 6; // 6 months.
+
+        using PublicKeyType = typename Kyber768Key::PublicKeyType;
 
       public:
         /// @brief Explicit constructor.
@@ -37,14 +40,84 @@ namespace HiveCom
         explicit Certificate(uint32_t version, const std::string &serial, const ByteView &publicKey,
                              const std::string &issuerName, const ByteView &privateKey, Dilithium3 &tool);
 
-        /// @brief Get the stored certificate.
-        /// @return The generated certificate.
+        /// @brief Explicit constructor.
+        /// This will extract all the necessary information from the certificate.
+        /// @param certificate The certificate data.
+        /// @param publicKey The issuer public key.
+        /// @param tool The dilithium-3 tool to verify.
+        explicit Certificate(std::string_view certificate, const ByteView &publicKey, Dilithium3 &tool);
+
+        /// @brief Get the stored full certificate.
+        /// @return The generated full certificate.
         [[nodiscard]] std::string_view getCertificate() const
         {
             return m_certificate;
         }
 
+        /// @brief Get the stored version.
+        /// @return The version.
+        [[nodiscard]] uint32_t getVersion() const
+        {
+            return m_version;
+        }
+
+        /// @brief Get the stored serial.
+        /// @return The serial.
+        [[nodiscard]] std::string_view getSerial() const
+        {
+            return m_serial;
+        }
+
+        /// @brief Get the stored issuer name.
+        /// @return The issuer name.
+        [[nodiscard]] std::string_view getIssuerName() const
+        {
+            return m_issuerName;
+        }
+
+        /// @brief Get the stored timestamp.
+        /// @return The timestamp.
+        [[nodiscard]] std::string_view getTimestamp() const
+        {
+            return m_timestamp;
+        }
+
+        /// @brief Get the stored public key.
+        /// @return The public key.
+        [[nodiscard]] const PublicKeyType &getPublicKey() const
+        {
+            return m_publicKey;
+        }
+
+        /// @brief Check if the certificate is valid or not.
+        /// @return True if the certificate is valid. Else false.
+        [[nodiscard]] bool isValid() const
+        {
+            return m_isValid;
+        }
+
       private:
+        /// @brief Split the certificate into multiple sub-strings by the `\n` character.
+        /// @param certificate The certificate to split.
+        /// @return The split string vector.
+        [[nodiscard]] std::vector<std::string> splitCertificate(std::string_view certificate) const;
+
+        /// @brief Check if the certificate's period is valid.
+        /// @param timestamp The certificate generation timestamp.
+        /// @return True if the time period is valid, else false.s
+        [[nodiscard]] bool isPeriodValid(std::string_view timestamp) const;
+
+      private:
+        PublicKeyType m_publicKey;
+
         std::string m_certificate;
+
+        std::string m_serial;
+        std::string m_issuerName;
+        std::string m_timestamp;
+
+        uint32_t m_version;
+
+        bool m_isValid = true;
     };
 } // namespace HiveCom
